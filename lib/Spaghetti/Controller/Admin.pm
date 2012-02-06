11aa06c2a5fb9de2078cee5faf5ef6c3a93ba948
Georgy Bazhukov <georgy.bazhukov@gmail.com>
2012-02-06 23:32:48 +0600

    add copyright

--- lib/Spaghetti/Controller/Admin.pm
+++ lib/Spaghetti/Controller/Admin.pm
@@ -17,3 +17,15 @@ use Mojo::Base 'Mojolicious::Controller';
         }
 
 1;
+
+__END__
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2012, Georgy Bazhukov.
+
+This program is free software, you can redistribute it and/or modify it under
+the terms of the Artistic License version 2.0.
+
+=cut
+
