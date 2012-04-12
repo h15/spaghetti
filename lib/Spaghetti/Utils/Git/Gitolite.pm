0061d285cc420d53100cb886c344e7737fb841d2
Georgy Bazhukov <georgy.bazhukov@gmail.com>
2012-04-12 10:26:33 +0600

    Fix gitolite module

--- lib/Spaghetti/Utils/Git/Gitolite.pm
+++ lib/Spaghetti/Utils/Git/Gitolite.pm
@@ -14,7 +14,13 @@ use Pony::Object;
                         FROM `repo` AS r
                         INNER JOIN `thread`  AS t ON ( r.id = t.id )
                         INNER JOIN `project` AS p ON ( p.id = t.topicId )
-                     }
+                     },
+                    getSshKeys =>
+                    q{
+                        SELECT s.key, s.id, u.name, u.id AS userId
+                        FROM `sshKey` AS s
+                        INNER JOIN `user` AS u ON (s.userId = u.id)
+                    },
                 };
     
     sub init
@@ -27,11 +33,28 @@ use Pony::Object;
         {
             my $this = shift;
             my $dbh  = Pony::Model::Dbh::MySQL->new->dbh;
-            my $sth  = $dbh->prepare( $this->sql->{getRepo} );
-               $sth->execute;
+            my $sth;
+            
+            # Get all keys.
+            # Map users to their keys.
+            
+            $sth  = $dbh->prepare( $this->sql->{getSshKeys} );
+            $sth->execute;
+            
+            my $keys = $sth->fetchall_hashref('id');
+            my %users;
+            
+            for my $k ( keys %$keys )
+            {
+                my $v = $keys->{$k};
+                push @{ $users{$v->{userId}} }, $k;
+            }
             
             # Get all repos.
             #
+            
+            $sth  = $dbh->prepare( $this->sql->{getRepo} );
+            $sth->execute;
             
             my $repos = $sth->fetchall_hashref('id');
             
@@ -43,13 +66,16 @@ use Pony::Object;
                 {
                     printf F "repo %s/%s\n", $r->{projectUrl}, $r->{url};
                     print  F "\tR\t= ALL\n";
-                    printf F "\tRW+CD\t= %s\n", $r->{owner};
+                    printf F "\tRW+CD\t= %s\n", join ' ', @{$users{$r->{owner}}};
                     
                     # Get user list from this repo.
                     #
                     
                     my @users = $model->list( {repoId => $r->{id}}, undef,
                                               'rwpcd', undef, 0, 10_000 );
+                    
+                    # Build rights.
+                    #
                     
                     my %rights;
                     
@@ -64,9 +90,14 @@ use Pony::Object;
                         push @{ $rights{$access} }, $u->{userId} if $access;
                     }
                     
+                    # Write users, which has permission.
+                    # Write users with the same rights on each iteration.
+                    
                     while ( my($k, $v) = each %rights )
                     {
-                        print F "\t$k\t= @$v\n";
+                        my @str = map { join ' ', @{$users{$_}} } @$v;
+                        
+                        print F "\t$k\t= @str\n";
                     }
                 }
             }
