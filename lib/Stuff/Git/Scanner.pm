5ff8110c6e41bd27503f4af5f176ed2bfac1dc22
Georgy Bazhukov <gosha@bugov.net>
2012-04-23 20:36:29 +0600

    add module for git access

--- lib/Stuff/Git/Scanner.pm
+++ lib/Stuff/Git/Scanner.pm
@@ -1,20 +1,55 @@
 package Stuff::Git::Scanner;
 use Pony::Object;
-use Git::Repository;
 
-    protected dir => undef;
+    use Git::Repository;
+    use Pony::Stash;
+    use Date::Parse;
+    
+    protected repo => undef;
     
     sub init : Public
         {
             my $this = shift;
-               $this->dir = shift;
+            my $proj = shift;
+            my $repo = shift;
+            my $dir  = Pony::Stash->get('gitdir');
             
-            die "Can't find dir " . $this->dir unless -d $this->dir;
+            die "Can't find dir $dir" unless -d $dir;
+            
+            $this->repo = Git::Repository->new( git_dir => "$dir/$proj/$repo.git" );
         }
     
     sub getLog : Public
         {
-        
+            my $this = shift;
+            my $size = shift;
+            my @log = $this->repo->run( qw/log -n/, $size );
+            my @logs;
+            
+            while ( @log )
+            {
+                my ( $comment ) = ( pop(@log) =~ /\s*(.*)/s );
+                
+                pop @log;
+                
+                my ( $date   ) = ( pop(@log) =~ /Date:\s*(.*)/s );
+                my ( $author ) = ( pop(@log) =~ /Author:\s*(.*)/s );
+                my ( $commit ) = ( pop(@log) =~ /commit\s*(.*)/s );
+                
+                my ( $name, $mail ) = ( $author =~ /(.*?)\<(.*?)\>/s );
+                
+                push @logs, {
+                                date   => str2time($date),
+                                name   => $name,
+                                mail   => $mail,
+                                commit => $commit,
+                                comment=> $comment
+                            };
+                
+                pop @log if @log;
+            }
+            
+            return @logs;
         }
     
     sub getFile : Public
