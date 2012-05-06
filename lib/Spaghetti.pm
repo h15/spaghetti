6635319a6dd9069d8790d821b24fc41615b30a06
Georgy Bazhukov <gosha@bugov.net>
2012-05-07 00:41:28 +0600

    get stash init from Spaghetti.pm

--- lib/Spaghetti.pm
+++ lib/Spaghetti.pm
@@ -4,7 +4,6 @@ use Mojo::Base 'Mojolicious';
     our $VERSION = '0.000011';
     our $COMMIT  = '';
     
-    use Pony::Stash;
     use Pony::Model::Crud;
     use Pony::Model::Dbh::MySQL;
     use Spaghetti::SQL;
@@ -25,66 +24,6 @@ use Mojo::Base 'Mojolicious';
                 
                 close C;
             }
-            
-            ##
-            ##  Configs
-            ##
-            
-            # Init configs in stash if they did not define.
-            #
-            
-            Pony::Stash->new('./resources/stash.dat');
-            Pony::Stash->findOrCreate(dbDriver => 'MySQL');
-            Pony::Stash->findOrCreate(thread => {size => 20});
-            Pony::Stash->findOrCreate(news => {size => 20});
-            Pony::Stash->findOrCreate(gitdir => '/home/git/repositories');
-            
-            Pony::Stash->findOrCreate
-            ( mail =>
-              {
-                from => 'no-reply@lorcode.org',
-                site => 'http://lorcode.org:3000',
-              }
-            );
-            
-            Pony::Stash->findOrCreate
-            ( user =>
-              {
-                cookies      => 'some random string',
-                salt         => 'some random string',
-                enable_registration => 1,
-                attempts     => 3,
-                mailAttempts => 3,
-                expairMail   => 86400,
-                sshKeyLimit  => 5,
-              }
-            );
-            
-            Pony::Stash->findOrCreate
-            ( defaultUserConf =>
-              {
-                isTreeView => 0,
-                lang  => 'en',
-                langs => 'en ru'
-              }
-            );
-            
-            Pony::Stash->findOrCreate
-            ( project =>
-              {
-                repos => 10,
-                topic => 1,
-              }
-            );
-            
-            Pony::Stash->findOrCreate
-            ( site =>
-              {
-                isAkismet => 0,
-                akismetApi => '',
-                root => 'http://lorcode.org/',
-              }
-            );
             
             # Preload lang hashes.
             #
