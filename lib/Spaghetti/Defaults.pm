e6684a9fd0147103f128b10f4463b4e54041f0f4
Gosha Bugov <gosha.bugov@gmail.com>
2012-07-06 08:16:36 +0600

    spaghetti goes to YAML

--- lib/Spaghetti/Defaults.pm
+++ lib/Spaghetti/Defaults.pm
@@ -1,76 +1,62 @@
 package Spaghetti::Defaults;
+use Pony::Object;
 
-    use Pony::Stash;
+    # Default config.
+    #
     
-    sub import
+    public conf =>
         {
-            ##
-            ##  Configs
-            ##
-            
-            # Init configs in stash if they did not define.
-            #
-            
-            Pony::Stash->new('./resources/stash.dat');
-            Pony::Stash->findOrCreate(dbDriver => 'MySQL');
-            Pony::Stash->findOrCreate(thread => {size => 20});
-            Pony::Stash->findOrCreate(news => {size => 20});
-            Pony::Stash->findOrCreate(gitdir => '/home/git/repositories');
-            
-            Pony::Stash->findOrCreate
-            ( git =>
-              {
-                all => 1,
-                dir => '/home/git/repositories',
-              }
-            );
-            
-            Pony::Stash->findOrCreate
-            ( mail =>
-              {
-                from => 'no-reply@lorcode.org',
-                site => 'http://lorcode.org:3000',
-              }
-            );
-            
-            Pony::Stash->findOrCreate
-            ( user =>
-              {
-                cookies      => 'some random string',
-                salt         => 'some random string',
-                enable_registration => 1,
-                attempts     => 3,
-                mailAttempts => 3,
-                expairMail   => 86400,
-                sshKeyLimit  => 5,
-              }
-            );
-            
-            Pony::Stash->findOrCreate
-            ( defaultUserConf =>
-              {
-                isTreeView => 1,
-                lang  => 'ru',
-                langs => 'ru'
-              }
-            );
-            
-            Pony::Stash->findOrCreate
-            ( project =>
-              {
-                repos => 10,
-                topic => 1,
-              }
-            );
-            
-            Pony::Stash->findOrCreate
-            ( site =>
-              {
-                isAkismet => 0,
-                akismetApi => '',
-                root => 'http://lorcode.org/',
-              }
-            );
-        }
+            dbDriver => 'MySQL',
+            thread =>
+            {
+              size => 20
+            },
+            news =>
+            {
+              size => 20
+            },
+            git =>
+            {
+              all => 1,
+              dir => '/home/git/repositories'
+            },
+            mail =>
+            {
+              from => 'no-reply@lorcode.org',
+              site => 'http://codewars.ru',
+            },
+            user =>
+            {
+              cookies => 'some random string',
+              salt => 'some random string',
+              enable_registration => 1,
+              attempts => 3,
+              mailAttempts => 3,
+              expairMail => 86400,
+              sshKeyLimit => 5,
+            },
+            defaultUserConf =>
+            {
+              isTreeView => 1,
+              lang => 'ru',
+              langs => [ qw/ru/ ],
+            },
+            project =>
+            {
+              repos => 10,
+              topic => 1,
+            },
+            site =>
+            {
+              isAkismet => 0,
+              akismetApi => '',
+              root => 'http://codewars.ru',
+            },
+            accessFilter =>
+            {
+              allow =>['192.168.105.*'],
+              deny =>['*.*.*.*']
+            }
+        };
 
 1;
